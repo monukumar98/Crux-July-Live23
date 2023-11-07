e720ae3a71dec90a3548f8ed2ef1772fdbd5462c
monukumar98 <monu012as@gmail.com>
2023-11-07 21:49:59 +0530

    Stack Qps

---
+++ Lec32/Histogram.java
@@ -0,0 +1,47 @@
+package Lec32;
+
+import java.util.Stack;
+
+public class Histogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = { 2, 3, 5, 4, 6, 1, 7 };
+		System.out.println(Area(arr));
+
+	}
+
+	public static int Area(int[] arr) {
+		int area = 0;
+		Stack<Integer> st = new Stack<>();
+		for (int i = 0; i < arr.length; i++) {
+			while (!st.isEmpty() && arr[i] < arr[st.peek()]) {
+				int h = arr[st.pop()];
+				int r = i;
+				if (st.isEmpty()) {
+					area = Math.max(area, h * r);
+				} else {
+					int l = st.peek();
+					area = Math.max(area, h * (r - l - 1));
+				}
+
+			}
+			st.push(i);
+
+		}
+		int r = arr.length;
+		while (!st.isEmpty()) {
+			int h = arr[st.pop()];
+
+			if (st.isEmpty()) {
+				area = Math.max(area, h * r);
+			} else {
+				int l = st.peek();
+				area = Math.max(area, h * (r - l - 1));
+			}
+
+		}
+		return area;
+	}
+
+}
